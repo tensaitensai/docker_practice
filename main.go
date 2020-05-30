0be96f43e1cd5ee0b5fb2ec07078b504fb16094d
tensaitensai <4619055.tus@gmail.com>
2020-05-30 23:16:10 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println(("received request"))
+		fmt.Fprintf(w, "Docker dayo!")
+	})
+	log.Println("start server")
+	server := &http.Server{Addr: "8080"}
+	if err := server.ListenAndServe(); err != nil {
+		log.Println((err))
+	}
+}
